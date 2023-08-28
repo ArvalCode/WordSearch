dd0684fd8fa73c00a023b44a9bd6fcefb8c6fc65
Nolan Tran <72712954+ArvalCode@users.noreply.github.com>
2023-08-27 22:51:06 -0500

    Create WordSearchRunner.java

---
+++ WordSearchRunner.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+import java.io.*;
+
+public class WordSearchRunner {
+
+    public static void main(String[] args) throws Exception {
+
+        WordSearch ms = new WordSearch(8, "APPLEXYPXLHJKEEDEGGLLXXCGFPDGOGNMYNTAHUUPUQDGBTSBTHIGHMSILKXLTHI");
+
+        String[] words = "APPLE AXE APEX CAT HEX EGG HAT COMPUTER GUM THIS TUG THIGH".split(" ");
+
+        System.out.println(ms.toString());
+
+        for (String s : words) {
+
+            if (ms.isFound(s)) {
+
+                System.out.println(s + " was found in the matrix.");
+
+            } else {
+
+                System.out.println(s + " was not found in the matrix.");
+
+            }
+
+        }
+
+    }
+
+}
